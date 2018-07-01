7882b9afd3488de52a0ccc3b72b0ab1b75d7a0fa
Varun Samuel <varun531@gmail.com>
2018-07-01 04:47:16 -0700

    Make qt+vulkan example work w/ bazel

---
+++ qt_example/hellovulkanwindow.h
@@ -0,0 +1,26 @@
+#include <QtGui/QVulkanWindow>
+
+class VulkanRenderer : public QVulkanWindowRenderer
+{
+
+public:
+    VulkanRenderer(QVulkanWindow *w);
+
+    void initResources() override;
+    void initSwapChainResources() override;
+    void releaseSwapChainResources() override;
+    void releaseResources() override;
+
+    void startNextFrame() override;
+
+private:
+    QVulkanWindow *m_window;
+    QVulkanDeviceFunctions *m_devFuncs;
+    float m_green = 0;
+};
+
+class VulkanWindow : public QVulkanWindow
+{
+public:
+    virtual QVulkanWindowRenderer *createRenderer() override;
+};
--- qt_example/qt.cpp
+++ qt_example/qt.cpp
@@ -2,8 +2,11 @@
  
 // Header file for input output functions
 #include <QtGui/QGuiApplication>
-//#include <QtGui/qvulkaninstance.h>
+#include <QtGui/qvulkaninstance.h>
 #include <QtGui/QWindow>
+#include <QtCore/QLoggingCategory>
+
+#include "hellovulkanwindow.h"
  
 using namespace std;
  
@@ -13,28 +16,27 @@ int main(int argc, char** argv)
 {
 	QGuiApplication app(argc, argv);
 
-	//QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
-    //QVulkanInstance inst;
+	QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
+  QVulkanInstance inst;
     
 
-     //printf("here");
-     /*inst.setLayers(QByteArrayList()
+  printf("here");
+     inst.setLayers(QByteArrayList()
                    << "VK_LAYER_GOOGLE_threading"
                    << "VK_LAYER_LUNARG_parameter_validation"
                    << "VK_LAYER_LUNARG_object_tracker"
                    << "VK_LAYER_LUNARG_core_validation"
                    << "VK_LAYER_LUNARG_image"
                    << "VK_LAYER_LUNARG_swapchain"
-                   << "VK_LAYER_GOOGLE_unique_objects");*/
+                   << "VK_LAYER_GOOGLE_unique_objects");
 
-    //printf("here");
-    /*if (!inst.create())
-        qFatal("Failed to create Vulkan instance: %d", inst.errorCode());*/
+    printf("here");
+    if (!inst.create())
+        qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
 
 
-	QWindow w;
-  	//VulkanWindow w;
-    //w.setVulkanInstance(&inst);
+  	VulkanWindow w;
+    w.setVulkanInstance(&inst);
 
     w.resize(1024, 768);
     w.show();
