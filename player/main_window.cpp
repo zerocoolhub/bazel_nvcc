3078e190fc938a4b4a0b10c4d4d91d58c4cb4381
Varun Samuel <varun531@gmail.com>
2018-07-12 00:17:10 -0700

    Add beginnings of file menu

--- player/main_window.cpp
+++ player/main_window.cpp
@@ -9,6 +9,8 @@
 MainWindow::MainWindow(VulkanWindow *w)
     : m_window(w)
 {
+    QWidget *widget = new QWidget;
+    setCentralWidget(widget);
 
     QWidget *wrapper = QWidget::createWindowContainer(w);
 
@@ -18,10 +20,17 @@ MainWindow::MainWindow(VulkanWindow *w)
     slider->setTickInterval(10);
     slider->setSingleStep(1);
 
-    QGridLayout *gridLayout = new QGridLayout(this);
+    QGridLayout *gridLayout = new QGridLayout(widget);
     gridLayout->addWidget(wrapper, 0, 0, 0);
     gridLayout->setColumnStretch(0, 0);
     gridLayout->setColumnStretch(1, 0);
     gridLayout->addWidget(slider, 1, 0, 0);
-    setLayout(gridLayout);
+    widget->setLayout(gridLayout);
+    createMenus();
+    setWindowTitle("Player");
+}
+
+void MainWindow::createMenus()
+{
+    fileMenu = menuBar()->addMenu(tr("&File"));
 }
--- player/main_window.h
+++ player/main_window.h
@@ -1,15 +1,19 @@
-#include <QtWidgets/QWidget>
+#include <QtWidgets/QMainWindow>
 
 class VulkanWindow;
 
+class QMenu;
 class QSlider;
 
-class MainWindow : public QWidget
+class MainWindow : public QMainWindow
 {
 public:
     explicit MainWindow(VulkanWindow *w);
 
 private:
+	QMenu *fileMenu;
     QSlider *slider;
     VulkanWindow *m_window;
+
+	void createMenus();
 };
