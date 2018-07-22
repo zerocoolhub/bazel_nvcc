b20e3005a520b6d9bc3a791c1a1985bff7fad475
Varun Samuel <varun531@gmail.com>
2018-07-22 06:03:51 -0700

    Add project to read decoded video frames

---
+++ video_frame/main.cpp
@@ -0,0 +1,14 @@
+// Simple C++ program to display "Hello World"
+ 
+// Header file for input output functions
+#include<iostream> 
+ 
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints heelo world
+    std::cout<<"Hello World";
+     
+    return 0;
+}
