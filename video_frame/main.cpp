54871029bb3063d24b3842c6b1083d90272e7ddb
Varun Samuel <varun531@gmail.com>
2018-07-22 06:22:19 -0700

    Write sample project to save decoded video frames as images

--- video_frame/main.cpp
+++ video_frame/main.cpp
@@ -2,13 +2,23 @@
  
 // Header file for input output functions
 #include<iostream> 
- 
-// main function -
-// where the execution of program begins
+#include <fstream>
+
+#include <opencv2/opencv.hpp>
+
+// Simply read the first frame in a decoded video and save it as an image via OpenCV,
+// to show that we can successfully deal with the decoded video as simple RGBA frames. 
 int main()
 {
-    // prints heelo world
-    std::cout<<"Hello World";
-     
-    return 0;
+  std::ifstream fpIn("/home/lex/bazel_nvcc/out.h264", std::ifstream::in | std::ifstream::binary);
+
+  int frameSize = 3686400; // 1280 * 720 * 4
+  std::unique_ptr<uint8_t[]> pHostFrame(new uint8_t[frameSize]);
+  std::streamsize nRead = fpIn.read(reinterpret_cast<char*>(pHostFrame.get()), frameSize).gcount();
+
+  cv::Mat imageWithData = cv::Mat(720, 1280, CV_8UC4, pHostFrame.get()).clone();
+  cvtColor(imageWithData, imageWithData, CV_BGR2RGBA);
+  cv::imwrite("/home/lex/cv.jpg", imageWithData);
+
+  return 0;
 }
