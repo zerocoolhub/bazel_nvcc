c159366c93c983fabf5757d69b98950e2668dfc5
Varun Samuel <varun531@gmail.com>
2018-07-23 19:10:42 -0700

    More style fixes

--- player/hellovulkantexture.cpp
+++ player/hellovulkantexture.cpp
@@ -861,78 +861,77 @@ void VulkanRenderer::releaseResources()
 
 void VulkanRenderer::startNextFrame()
 {
-    VkDevice dev = m_window->device();
-    VkCommandBuffer cb = m_window->currentCommandBuffer();
-    const QSize sz = m_window->swapChainImageSize();
+  VkDevice dev = m_window->device();
+  VkCommandBuffer cb = m_window->currentCommandBuffer();
+  const QSize sz = m_window->swapChainImageSize();
 
-    // Add the necessary barriers and do the host-linear -> device-optimal copy, if not yet done.
-    //ensureTexture();
+  // Add the necessary barriers and do the host-linear -> device-optimal copy, if not yet done.
+  //ensureTexture();
 
-    if (m_shouldUpdate) {
-      m_shouldUpdate = false;
-      QImage nextFrame(m_frameData, 1280, 720, 5120, QImage::Format_RGBA8888);
-      transitionImageLayout(m_texImage, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL);
-      writeLinearImage(nextFrame, m_texImage, m_texMem);
-      delete m_frameData;
-    //QImage img(QStringLiteral("/home/lex/bazel_nvcc/player/texture1.png"));
+  if (m_shouldUpdate) {
+    m_shouldUpdate = false;
+    QImage nextFrame(m_frameData, 1280, 720, 5120, QImage::Format_RGBA8888);
+    transitionImageLayout(m_texImage, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL);
+    writeLinearImage(nextFrame, m_texImage, m_texMem);
+    delete m_frameData;
   }
 
-    VkClearColorValue clearColor = {{ 0, 0, 0, 1 }};
-    VkClearDepthStencilValue clearDS = { 1, 0 };
-    VkClearValue clearValues[2];
-    memset(clearValues, 0, sizeof(clearValues));
-    clearValues[0].color = clearColor;
-    clearValues[1].depthStencil = clearDS;
+  VkClearColorValue clearColor = {{ 0, 0, 0, 1 }};
+  VkClearDepthStencilValue clearDS = { 1, 0 };
+  VkClearValue clearValues[2];
+  memset(clearValues, 0, sizeof(clearValues));
+  clearValues[0].color = clearColor;
+  clearValues[1].depthStencil = clearDS;
 
-    VkRenderPassBeginInfo rpBeginInfo;
-    memset(&rpBeginInfo, 0, sizeof(rpBeginInfo));
-    rpBeginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-    rpBeginInfo.renderPass = m_window->defaultRenderPass();
-    rpBeginInfo.framebuffer = m_window->currentFramebuffer();
-    rpBeginInfo.renderArea.extent.width = sz.width();
-    rpBeginInfo.renderArea.extent.height = sz.height();
-    rpBeginInfo.clearValueCount = 2;
-    rpBeginInfo.pClearValues = clearValues;
-    VkCommandBuffer cmdBuf = m_window->currentCommandBuffer();
-    m_devFuncs->vkCmdBeginRenderPass(cmdBuf, &rpBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
+  VkRenderPassBeginInfo rpBeginInfo;
+  memset(&rpBeginInfo, 0, sizeof(rpBeginInfo));
+  rpBeginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+  rpBeginInfo.renderPass = m_window->defaultRenderPass();
+  rpBeginInfo.framebuffer = m_window->currentFramebuffer();
+  rpBeginInfo.renderArea.extent.width = sz.width();
+  rpBeginInfo.renderArea.extent.height = sz.height();
+  rpBeginInfo.clearValueCount = 2;
+  rpBeginInfo.pClearValues = clearValues;
+  VkCommandBuffer cmdBuf = m_window->currentCommandBuffer();
+  m_devFuncs->vkCmdBeginRenderPass(cmdBuf, &rpBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
 
-    quint8 *p;
-    VkResult err = m_devFuncs->vkMapMemory(dev, m_bufMem, m_uniformBufInfo[m_window->currentFrame()].offset,
-            UNIFORM_DATA_SIZE, 0, reinterpret_cast<void **>(&p));
-    if (err != VK_SUCCESS)
-        qFatal("Failed to map memory: %d", err);
-    QMatrix4x4 m = m_proj;
-    // m.rotate(m_rotation, 0, 0, 1); // DISABLE ROTATION
-    memcpy(p, m.constData(), 16 * sizeof(float));
-    m_devFuncs->vkUnmapMemory(dev, m_bufMem);
+  quint8 *p;
+  VkResult err = m_devFuncs->vkMapMemory(dev, m_bufMem, m_uniformBufInfo[m_window->currentFrame()].offset,
+          UNIFORM_DATA_SIZE, 0, reinterpret_cast<void **>(&p));
+  if (err != VK_SUCCESS)
+      qFatal("Failed to map memory: %d", err);
+  QMatrix4x4 m = m_proj;
+  // m.rotate(m_rotation, 0, 0, 1); // DISABLE ROTATION
+  memcpy(p, m.constData(), 16 * sizeof(float));
+  m_devFuncs->vkUnmapMemory(dev, m_bufMem);
 
-    // Not exactly a real animation system, just advance on every frame for now.
-    m_rotation += 1.0f;
+  // Not exactly a real animation system, just advance on every frame for now.
+  m_rotation += 1.0f;
 
-    m_devFuncs->vkCmdBindPipeline(cb, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
-    m_devFuncs->vkCmdBindDescriptorSets(cb, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipelineLayout, 0, 1,
-                               &m_descSet[m_window->currentFrame()], 0, nullptr);
-    VkDeviceSize vbOffset = 0;
-    m_devFuncs->vkCmdBindVertexBuffers(cb, 0, 1, &m_buf, &vbOffset);
+  m_devFuncs->vkCmdBindPipeline(cb, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
+  m_devFuncs->vkCmdBindDescriptorSets(cb, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipelineLayout, 0, 1,
+                             &m_descSet[m_window->currentFrame()], 0, nullptr);
+  VkDeviceSize vbOffset = 0;
+  m_devFuncs->vkCmdBindVertexBuffers(cb, 0, 1, &m_buf, &vbOffset);
 
-    VkViewport viewport;
-    viewport.x = viewport.y = 0;
-    viewport.width = sz.width();
-    viewport.height = sz.height();
-    viewport.minDepth = 0;
-    viewport.maxDepth = 1;
-    m_devFuncs->vkCmdSetViewport(cb, 0, 1, &viewport);
+  VkViewport viewport;
+  viewport.x = viewport.y = 0;
+  viewport.width = sz.width();
+  viewport.height = sz.height();
+  viewport.minDepth = 0;
+  viewport.maxDepth = 1;
+  m_devFuncs->vkCmdSetViewport(cb, 0, 1, &viewport);
 
-    VkRect2D scissor;
-    scissor.offset.x = scissor.offset.y = 0;
-    scissor.extent.width = viewport.width;
-    scissor.extent.height = viewport.height;
-    m_devFuncs->vkCmdSetScissor(cb, 0, 1, &scissor);
+  VkRect2D scissor;
+  scissor.offset.x = scissor.offset.y = 0;
+  scissor.extent.width = viewport.width;
+  scissor.extent.height = viewport.height;
+  m_devFuncs->vkCmdSetScissor(cb, 0, 1, &scissor);
 
-    m_devFuncs->vkCmdDraw(cb, 4, 1, 0, 0);
+  m_devFuncs->vkCmdDraw(cb, 4, 1, 0, 0);
 
-    m_devFuncs->vkCmdEndRenderPass(cmdBuf);
+  m_devFuncs->vkCmdEndRenderPass(cmdBuf);
 
-    m_window->frameReady();
-    m_window->requestUpdate(); // render continuously, throttled by the presentation rate
+  m_window->frameReady();
+  m_window->requestUpdate(); // render continuously, throttled by the presentation rate
 }
